ca4c8199dfd20f23406129b0b4f03704b9b427d6
cdp53190 <cdp53190@gmail.com>
2018-04-11 17:00:04 +0900

    Initial Commit

---
+++ MetersConversionTable/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  MetersConversionTable
+//
+//  Created by Eiichi Hayashi on 2018/04/11.
+//  Copyright © 2018年 skyElements. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ MetersConversionTable/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MetersConversionTable
+//
+//  Created by Eiichi Hayashi on 2018/04/11.
+//  Copyright © 2018年 skyElements. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
